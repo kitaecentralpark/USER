ae8d36c26291c2a8d8a451914cb33564321b995e
kitaecentralpark <73973770+kitaecentralpark@users.noreply.github.com>
2020-11-26 13:33:38 +0900

    update11/26

--- app/src/main/java/com/example/user/MapsActivity.java
+++ app/src/main/java/com/example/user/MapsActivity.java
@@ -1,8 +1,12 @@
 package com.example.user;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,10 +14,23 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
 
     private GoogleMap mMap;
+    Button btnRevoke, btnLogout;
+    private FirebaseAuth mAuth ;
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+    private ChildEventListener mChildEventListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +40,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        btnLogout = (Button)findViewById(R.id.btn_logout);
+        btnRevoke = (Button)findViewById(R.id.btn_revoke);
+        btnLogout.setOnClickListener(this);
+        btnRevoke.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
     }
+
 
     /**
      * Manipulates the map once available.
@@ -38,9 +62,62 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        final LatLng loc = new LatLng (37,50);
+        mMap.addMarker(new MarkerOptions().position(loc).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(loc));
+
+        databaseReference.child("gps").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                location newlocation = snapshot.getValue(location.class);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
+
+    private void signOut() {
+        FirebaseAuth.getInstance().signOut();
+    }
+
+    private void revokeAccess() {
+        mAuth.getCurrentUser().delete();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_logout:
+                signOut();
+                finishAffinity();
+                break;
+            case R.id.btn_revoke:
+                revokeAccess();
+                finishAffinity();
+                break;
+        }
+    }
+
 }
